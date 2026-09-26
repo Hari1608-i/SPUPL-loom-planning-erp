8e39dde63c24d4347149d81732ed0e01c988b764
Hari1608-i <hari2025@gmail.com>
2026-09-26 21:46:52 +0530

    Force Prisma to utilize active connection string in prismaClient utility

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -2,17 +2,25 @@ const { PrismaClient } = require('@prisma/client');
 
 let prisma;
 
+const connectionString = process.env.DATABASE_URL || process.env.DIRECT_URL;
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient({
     datasources: {
       db: {
-        url: process.env.DATABASE_URL,
+        url: connectionString,
       },
     },
   });
 } else {
   if (!global.globalPrisma) {
-    global.globalPrisma = new PrismaClient();
+    global.globalPrisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: connectionString,
+        },
+      },
+    });
   }
   prisma = global.globalPrisma;
 }
